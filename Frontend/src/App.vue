4622a6e098620ce4f90dca6e4239b483932c539e
NeoCrawler <neocrawler@hotmail.com>
2026-01-18 12:23:10 +0100

    Rebuild front-end project
    [Changed]
    Recreated the frontend project because tailwind was still giving issues.
    Opted to search online for setup rather than the documentation this time.

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,41 +1,11 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-async function TestConnection()
-{
-    try 
-    {
-        const response = await fetch('https://localhost:7033/api/Task/GetAll');
-        
-        if (!response.ok) throw new Error('Network response was not ok');
-        
-        const data = await response.json();
-        console.log(data);
-
-    } catch (error) 
-    {
-        console.error('Fetch error:', error);
-    }
-}
-
-onMounted(() =>
-{
-  TestConnection();
-})
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex h-screen items-start bg-gray-900">
+    <h2>Test Please?</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
